9a29aaf94370f0d926520edbe4db012ac1aabfd5
Vinit Mepani <vintimepani2712@gmail.com>
2024-04-19 16:28:37 +0530

    commit

--- lib/pages/mainscreen.dart
+++ lib/pages/mainscreen.dart
@@ -28,7 +28,9 @@ import 'package:all_widget/pages/tooltip_widget.dart';
 import 'package:all_widget/pages/wrap_widget.dart';
 
 class MainScreenPage extends StatelessWidget {
-  MainScreenPage({super.key,});
+  MainScreenPage({
+    super.key,
+  });
 
   final List<Map<String, dynamic>> _widgetList = [
     {
@@ -38,7 +40,7 @@ class MainScreenPage extends StatelessWidget {
     },
     {
       "text": "AppBar Widget",
-      "widget":  AppBarWidget(),
+      "widget": AppBarWidget(),
       "iconData": Icons.flutter_dash_sharp,
     },
     {
@@ -46,7 +48,6 @@ class MainScreenPage extends StatelessWidget {
       "widget": const AspectRatioWidget(),
       "iconData": Icons.flutter_dash_sharp,
     },
-   
     {
       "text": "Button Widget",
       "widget": const ButtonWidget(),
@@ -171,105 +172,99 @@ class MainScreenPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      elevation: 5,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text(
-            'Flutter Widget List',
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Color.fromARGB(255, 0, 0, 0)),
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xFFE2EDF7), Color(0xFFB5C3D3)],
           ),
         ),
-        body: GridView.builder(
-          padding: const EdgeInsets.all(20),
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 2,
-            crossAxisSpacing: 20,
-            mainAxisSpacing: 20,
-          ),
-          itemCount: _widgetList.length,
-          itemBuilder: (context, index) {
-            return _buildTextButton(
-              text: _widgetList[index]["text"],
-              iconData: _widgetList[index]["iconData"],
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => _widgetList[index]["widget"],
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  'Flutter All Widget',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.blueAccent,
+                    fontFamily: 'Roboto',
                   ),
-                );
-              },
-            );
-          },
+                ),
+              ),
+              Expanded(
+                child: GridView.builder(
+                  padding: const EdgeInsets.all(20),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 20,
+                    mainAxisSpacing: 20,
+                  ),
+                  itemCount: _widgetList.length,
+                  itemBuilder: (context, index) {
+                    return _buildCard(
+                      text: _widgetList[index]["text"],
+                      iconData: _widgetList[index]["iconData"],
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => _widgetList[index]["widget"],
+                          ),
+                        );
+                      },
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Widget _buildTextButton({
+  Widget _buildCard({
     required String text,
     required IconData iconData,
     required VoidCallback onPressed,
   }) {
-    return Container(
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 255, 255, 255),
-        borderRadius: BorderRadius.circular(10),
-        boxShadow: [
-          BoxShadow(
-            color: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.5),
-            spreadRadius: 2,
-            blurRadius: 5,
-            offset: const Offset(0, 2),
-          ),
-        ],
+    return Card(
+      elevation: 8,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
       ),
-      child: SizedBox(
-        width: 180,
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Container(
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: const Color.fromARGB(255, 255, 255, 255),
+      child: InkWell(
+        onTap: onPressed,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                iconData,
+                color: Colors.blue,
+                size: 40,
               ),
-              width: double.infinity,
-              height: double.infinity,
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(
-                  iconData,
-                  color: const Color.fromARGB(162, 73, 39, 224),
-                  size: 30,
-                ),
-                const SizedBox(height: 10),
-                Center(
-                  child: Text(
-                    text,
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(
-                      color: Color.fromARGB(255, 0, 0, 0),
-                      fontFamily: "Times New Roman",
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            Positioned.fill(
-              child: Material(
-                color: Colors.transparent,
-                child: InkWell(
-                  onTap: onPressed,
+              const SizedBox(height: 10),
+              Text(
+                text,
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Roboto',
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
