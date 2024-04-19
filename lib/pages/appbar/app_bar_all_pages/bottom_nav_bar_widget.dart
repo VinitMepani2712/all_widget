5f436fc47f6c6eb88d40fe5b67b5bfb449e4e275
Vinit Mepani <vintimepani2712@gmail.com>
2024-04-19 16:07:31 +0530

    commit

--- lib/pages/appbar/app_bar_all_pages/bottom_nav_bar_widget.dart
+++ lib/pages/appbar/app_bar_all_pages/bottom_nav_bar_widget.dart
@@ -17,7 +17,7 @@ class BottomNavigationAppBar extends StatelessWidget {
           children: [
             Container(
               decoration: BoxDecoration(
-                color: Color.fromARGB(255, 206, 122, 122),
+                color: const Color.fromARGB(255, 206, 122, 122),
                 borderRadius: BorderRadius.circular(20),
               ),
               child: TextButton(
@@ -63,11 +63,11 @@ class BottomNavigationAppBar extends StatelessWidget {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('Bottom Navigation App Bar Code'),
+          title: const Text('Bottom Navigation App Bar Code'),
           content: SingleChildScrollView(
             child: ListBody(
               children: [
-                SizedBox(height: 20),
+                const SizedBox(height: 20),
                 Container(
                   color: Colors.grey.shade200,
                   child: BottomNavigationBar(
@@ -94,7 +94,7 @@ class BottomNavigationAppBar extends StatelessWidget {
           ),
           actions: <Widget>[
             TextButton(
-              child: Text('Close'),
+              child: const Text('Close'),
               onPressed: () {
                 Navigator.of(context).pop();
               },
@@ -106,77 +106,111 @@ class BottomNavigationAppBar extends StatelessWidget {
   }
 
   final String _bottomNavAppBarCode = '''
-// Example code for Bottom Navigation App Bar
-
+import 'package:all_widget/codepage/show_code_file.dart';
+import 'package:all_widget/textstyle/textstyle.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Bottom Navigation App Bar',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _selectedIndex = 0;
-
-  static List<Widget> _widgetOptions = <Widget>[
-    Text('Home Page'),
-    Text('Search Page'),
-    Text('Profile Page'),
-  ];
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
+class BottomNavigationAppBar extends StatelessWidget {
+  const BottomNavigationAppBar({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Bottom Navigation App Bar'),
-      ),
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const <BottomNavigationBarItem>[
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            label: 'Search',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.blue,
-        onTap: _onItemTapped,
+      appBar: AppBar(title: const Text('Bottom Navigation App Bar')),
+      body: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(255, 206, 122, 122),
+                borderRadius: BorderRadius.circular(20),
+              ),
+              child: TextButton(
+                onPressed: () {
+                  _showBottomNavAppBarCode(context);
+                },
+                child: Text(
+                  'Show Bottom Navigation App Bar',
+                  style: AppWidget.textStyle(),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            TextButton(
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all(Colors.black26),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => CodePage(
+                      code: _bottomNavAppBarCode,
+                    ),
+                  ),
+                );
+              },
+              child: Center(
+                child: Text(
+                  'Show Code',
+                  style: AppWidget.textStyle(),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
-}
+
+  void _showBottomNavAppBarCode(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Bottom Navigation App Bar Code'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: [
+                const SizedBox(height: 20),
+                Container(
+                  color: Colors.grey.shade200,
+                  child: BottomNavigationBar(
+                    items: const <BottomNavigationBarItem>[
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.home),
+                        label: 'Home',
+                      ),
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.search),
+                        label: 'Search',
+                      ),
+                      BottomNavigationBarItem(
+                        icon: Icon(Icons.person),
+                        label: 'Profile',
+                      ),
+                    ],
+                    currentIndex: 0,
+                    onTap: (index) {},
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Close'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
 ''';
 }
